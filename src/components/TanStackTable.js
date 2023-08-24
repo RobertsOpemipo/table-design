88b75d420674247432ea04af38d79b5b4a2fece7
Opemipo Roberts <opemipo.roberts@yahoo.co.uk>
2023-08-24 02:22:02 +0100

    Table

--- src/components/TanStackTable.js
+++ src/components/TanStackTable.js
@@ -1,6 +1,9 @@
-import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { createColumnHelper, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
 import { USERS } from "../data";
 import { useState } from "react";
+import DownloadBtn from "./DownloadBtn";
+import { DebouncedInput } from "./DebouncedInput";
+import { SearchIcon } from "../Icons/Icons";
 
 
 const TanStackTable = () => {
@@ -17,7 +20,7 @@ const TanStackTable = () => {
         columnHelper.accessor("profile",{
             cell:(info)=>(
                 <img 
-                    src={info.getValue()} 
+                    src={info?.getValue()} 
                     alt="..." 
                     className="rounded-full w-10 h-10 object-cover" 
                 />
@@ -50,15 +53,22 @@ const TanStackTable = () => {
             cell: (info)=> <span>{info.getValue()}</span>,
             header:'Progress',
         }),
-    ]
+    ];
 
 
     const [data] = useState(() => [...USERS]);
+    const [globalFilter,setGlobalFilter] = useState("");
+
 
     const table = useReactTable({
         data,
         columns,
+        state:{
+            globalFilter,
+        },
+        getFilteredRowModel: getFilteredRowModel(),
         getCoreRowModel:getCoreRowModel(),
+        getPaginationRowModel:getPaginationRowModel()
     })
     
     return (
@@ -66,14 +76,28 @@ const TanStackTable = () => {
 
 
         <div className="p-2 max-w-5xl mx-auto text-white fill-gray-400">
-            <table>
-                <thead>
+            
+            <div className="flex justify-between mb-2">
+                <div className="w-full flex items-center gap-1">
+                    <SearchIcon />
+                    <DebouncedInput 
+                    value={globalFilter ?? ""} 
+                    onChange={(value) => setGlobalFilter (String(value))} 
+                    className="p-2 bg-transparent outline-none border-b-2 w-1/5 focus:w-1/3 duration-300 border-blue-800"    
+                    placeholder = "Search all columns"
+                    />
+                </div>
+                
+                <DownloadBtn data={data} fileName={"peoples"}/>
+            </div>
+            <table className="border border-gray-800 w-full text-left">
+                <thead className="bg-white text-gray-900">
                     {
                         table.getHeaderGroups().map((headerGroup)=>(
                             <tr key={headerGroup.id}>
                                 {
                                     headerGroup.headers.map(header=>(
-                                        <th key={header.id}>
+                                        <th key={header.id} className="capitalize px-3.5 py-2">
                                             {
                                                 flexRender(
                                                     header.column.columnDef.header,
@@ -89,15 +113,81 @@ const TanStackTable = () => {
                 </thead>
 
                 <tbody>
-                    {
-                        table.getRowModel().rows.length ? (
-                            table.getRowModel().rows.map((row,i) => (
-                                <tr key={row.id}></tr>
+                    {table.getRowModel().rows.length 
+                    ? (table.getRowModel().rows.map((row,i) => (
+                                <tr key={row.id} className={`
+                                    ${i % 2 === 0 ? "bg-gray-900": "bg-gray-800"}
+                                `}>
+                                    {row.getVisibleCells().map((cell) =>(
+                                        <td key={cell.id} className="px-3.5 py-2">
+                                            {
+                                                flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext()
+                                                )}
+                                        </td>
+                                    ))}
+                                </tr>
                             ))
                         ) : null                    
                     }
                 </tbody>
             </table>
+            {/* pagination */}
+            <div className="flex items-center justify-end mt-2 gap-2">
+                <button 
+                    onClick={()=>{
+                        table.previousPage();
+                    }} 
+                    disabled={!table.getCanPreviousPage()}
+                    className="p-1 border-gray-900 px-2 disabled:opacity-30">
+                        {"<"}
+                </button>
+                <button 
+                    onClick={()=>{
+                        table.nextPage();
+                    }} 
+                    disabled={!table.getCanNextPage()}
+                    className="p-1 border-gray-900 px-2 disabled:opacity-30">
+                        {">"}
+                </button>
+
+                <span className="flex items-center gap-1">
+                    <div>
+                        Page
+                    </div>
+                    <strong>
+                        {table.getState().pagination.pageIndex + 1} of {""} {table.getPageCount()}
+                    </strong>
+                </span>
+
+                <span className="flex items-center gap-1">
+                    Go to Page: 
+                    <input 
+                        type="number"
+                        defaultValue={table.getState().pagination.pageIndex + 1}
+                        onChange={(e)=>{
+                            const page = e.target.value ? Number(e.target.value)-1 : 0
+                            table.setPageIndex(page)
+                        }}
+                        className="border p-1 rounded w-16 bg-transparent" 
+                    />
+
+
+                </span>
+                <select value={table.getState().pagination.pageSize} onChange={(e)=>{
+                    table.setPageSize(Number(e.target.value));
+                }}
+                className="p-2 bg-transparent text-gray-950"
+                
+                >
+                    {[10,20,30,50].map((pageSize)=>(
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </div>
     );
 };
